d72c2dc97af46cad4634255c6775d07bff49587b
lucaspagliarin <lucas.pagliarin@gmail.com>
2023-05-20 20:03:04 -0300

    Upload de imagem e Registro de Memória no Backend

---
+++ src/components/MediaPicker.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { ChangeEvent, useState } from 'react'
+
+export function MediaPicker() {
+  const [preview, setPreview] = useState<string | null>(null)
+
+  function onMediaSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.target
+
+    if (!files) {
+      return
+    }
+
+    const previewURL = URL.createObjectURL(files[0])
+
+    setPreview(previewURL)
+  }
+
+  return (
+    <>
+      <input
+        onChange={onMediaSelected}
+        name="coverUrl"
+        type="file"
+        id="media"
+        className="invisible h-0 w-0"
+        accept="image/*"
+      />
+
+      {preview && (
+        // eslint-disable-next-line
+        <img
+          src={preview}
+          alt=""
+          className="aspect-video w-full rounded-lg object-cover"
+        />
+      )}
+    </>
+  )
+}
